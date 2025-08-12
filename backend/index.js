b9eb341a493a7c4ffab4e943220aa34d2f88f76b
Sarth Verma <sarverjii@gmail.com>
2025-08-12 16:54:09 +0530

    Updated Backend CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,21 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 const PlayerRouter = require("./Route/Player.route.js");
 const MemberRouter = require("./Route/Member.route.js");
 const EventRouter = require("./Route/Event.route.js");
